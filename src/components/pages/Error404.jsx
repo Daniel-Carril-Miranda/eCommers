dc31d01b84efdcd63be24aeb2ae692eb2e2f5c44
Daniel <danielcarrilmiranda@alumnos.tuniversformacion.es>
2024-02-29 13:22:42 +0100

    Hacemos más cositas

---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,11 @@
+export const Error404 = () => {
+  return (
+    <>
+    <h1>Error 404</h1>
+    <p>La página no existe</p>
+    </>
+  )
+}
+
+// rafc -> coamando rápido para crearlo
+// Hay que ponerlo entre divs o entre fragmentos como en este caso
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { API_URL } from "../../constants/env"
+export const Home = () => {
+  return (
+    <div>
+        <h1>Bienvenido/a {API_URL} </h1>
+        <p>Este es el sitio en desarrollo</p>
+    </div>
+  )
+}
+
+export default Home
